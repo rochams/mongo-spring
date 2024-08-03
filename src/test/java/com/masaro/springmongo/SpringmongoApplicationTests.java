8b5f764bfa039887149cef0ed3777071be8ec620
rochams <msrocha.ad@gmail.com>
2024-08-03 10:59:54 -0300

    commit inicial

---
+++ src/test/java/com/masaro/springmongo/SpringmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masaro.springmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
